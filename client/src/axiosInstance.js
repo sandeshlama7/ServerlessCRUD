1824188ad2868c43f8150a370dec13bcd9fb91a9
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-08-04 20:43:10 +0545

    Added quotes around the base url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
 //   baseURL: process.env.API_BASE_URL,
-    baseURL: http://localhost:8800,
+    baseURL: "http://localhost:8800",
 });
 
 export default axios;
