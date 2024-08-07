b23643fae443f9390ad7830820f9f44aeb9043fa
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-08-07 09:58:55 +0545

    Commenting the alarm file

--- Terraform/cloudwatch.tf
+++ Terraform/cloudwatch.tf
@@ -1,5 +1,6 @@
-module "rds_alarm" {
-  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
-  
-
-}
+# module "rds_alarm" {
+#   source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+#   alarm_name = "{module.rds.db_instance_identifier}-alarm"
+#   comparison_operator =
+#   evaluation_periods =
+# }
