a3f779c8fb91e554294515fb09d8337eb956676a
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-08-07 11:06:21 +0545

    Commenting the unfinished alarm metric

--- Terraform/cloudwatch.tf
+++ Terraform/cloudwatch.tf
@@ -1,11 +1,11 @@
-module "rds_alarm" {
-  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
-  alarm_name = "{module.rds.db_instance_identifier}-alarm"
-  comparison_operator =
-  evaluation_periods =
-  alarm_actions = [module.sns_topic.topic_arn]
+# module "rds_alarm" {
+#   source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarm"
+#   alarm_name = "{module.rds.db_instance_identifier}-alarm"
+#   comparison_operator =
+#   evaluation_periods =
+#   alarm_actions = [module.sns_topic.topic_arn]
 
-  dimensions = {
-    DBInstanceIdentifier = module.rds.db_instance_identifier
-  }
-}
+#   dimensions = {
+#     DBInstanceIdentifier = module.rds.db_instance_identifier
+#   }
+# }
