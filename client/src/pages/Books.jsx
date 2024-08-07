743b0c96e747359e282dfa77e05a7ba8b6368cde
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-08-07 13:35:51 +0545

    Changing the h1 Name

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -37,7 +37,7 @@ const Books = () => {
 
   return (
     <div>
-      <h1>Lama Book Shop</h1>
+      <h1>Mama Book Shop</h1>
       <div className="books">
         {books.map((book) => (
           <div key={book.id} className="book">
